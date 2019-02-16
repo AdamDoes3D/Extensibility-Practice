a24aa52ba6b067e709c014ee2c388f59182d503e
Adam Rhodes <jrhodes1183@gmail.com>
2019-02-16 15:57:32 -0600

    Created program to practice extensibility of code.

---
+++ Extensibility/ILogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extensibility
+{
+    interface ILogger
+    {
+        void LogError(string message);
+        void LogInfo(string message);
+    }
+}
